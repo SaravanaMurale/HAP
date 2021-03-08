5138c03a5bfe0945371e7c3ecc78cc3d287ff1da
murali <murali.e@sosaley.in>
2021-03-08 18:37:42 +0530

    Initial commit

---
+++ app/src/main/java/com/sosaley/hap/register/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.sosaley.hap.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.sosaley.hap.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
